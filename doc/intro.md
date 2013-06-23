d4144950391ead357d43a24bd72a5ce7f87622e6
Alex Bahouth <abahouth@onekingslane.com>
2013-06-23 16:59:54 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aws-clj-sdk
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/aws_clj_sdk/core_test.clj
@@ -0,0 +1,7 @@
+(ns aws-clj-sdk.core-test
+  (:use clojure.test
+        aws-clj-sdk.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
