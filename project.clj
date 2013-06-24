cf11fb3858adf59656f44ee0034a0ea32b0731c7
Alex Bahouth <abahouth@onekingslane.com>
2013-06-23 17:02:21 -0700

    typo.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aws-clj-sdk "0.1.0-SNAPSHOT"
   :description "Clojure Wrapper around Amazon AWS Java SDK"
-  :url "http://github.com/roxxi/aws-clj-sdk"
+  :url "http://github.com/Roxxi/aws-clj-sdk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
