75132afc8e074e41fdc65281e2b37faf043b9f1f
Alex Bahouth <abahouth@onekingslane.com>
2013-06-23 17:01:05 -0700

    Added aws library and description.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject aws-clj-sdk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure Wrapper around Amazon AWS Java SDK"
+  :url "http://github.com/roxxi/aws-clj-sdk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.amazonaws/aws-java-sdk "1.4.7"]])
