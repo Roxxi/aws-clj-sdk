3b9a1d2e921af5fec07fd431f22e76f58fe887fb
Alex Bahouth <abahouth@onekingslane.com>
2013-06-23 17:03:29 -0700

    Added org of "roxxi"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aws-clj-sdk "0.1.0-SNAPSHOT"
+(defproject roxxi/aws-clj-sdk "0.1.0-SNAPSHOT"
   :description "Clojure Wrapper around Amazon AWS Java SDK"
   :url "http://github.com/Roxxi/aws-clj-sdk"
   :license {:name "Eclipse Public License"
